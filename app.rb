1a0e72cfc75d2a8a39cf9a4b8cd38522d87ade07
Jaime Vargas <83519750+jaimevargas315@users.noreply.github.com>
2024-02-21 00:52:55 +0000

    passed all tests

--- app.rb
+++ app.rb
@@ -7,3 +7,43 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/square/new") do
+
+  erb(:square)
+end
+
+get("/square/square_results")do
+
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+
+  erb(:square_root)
+end
+
+get("/square_root/square_root_results") do
+
+  erb(:square_root_results)
+end
+
+get("/random/new") do
+
+  erb(:random)
+end
+
+get("/random/random_results") do
+
+  erb(:random_results)
+end
+
+get("/payment/new") do
+
+  erb(:payment)
+end
+
+get("/payment/payment_results") do
+
+  erb(:payment_results)
+end
